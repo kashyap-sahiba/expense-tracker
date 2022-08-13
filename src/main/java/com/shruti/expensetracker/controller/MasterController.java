00bc6969571077a31b7d864dcb358b6c21b4ff77
shrutiKashyap23 <shrutikashyapofficial23@gmail.com>
2022-08-13 21:42:12 +0530

    Edit feature handler method

--- src/main/java/com/shruti/expensetracker/controller/MasterController.java
+++ src/main/java/com/shruti/expensetracker/controller/MasterController.java
@@ -40,9 +40,13 @@ public class MasterController {
         return "redirect:/";
     }
 
-    @RequestMapping(value = "/expense/{id}", method = RequestMethod.POST)
-    public String edit(@PathVariable("id") Long id) {
-       return "redirect:/";
+    @RequestMapping(value = "/expense/{id}")
+    public ModelAndView edit(@PathVariable("id") Long id) {
+        ModelAndView mav = new ModelAndView("expense");
+        Expense expense = expenseService.findById(id);
+        mav.addObject("expense",expense);
+       return mav;
     }
+
 
 }
